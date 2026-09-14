ab6d530ac89af73be269994b8aa87bb763fc1da4
eivin monroy <lopezeivin1077@gmail.com>
2026-09-14 01:26:15 -0600

    Version_02: (recursividades)

--- Recursividad1/src/recursividad1/Recursividad1.java
+++ Recursividad1/src/recursividad1/Recursividad1.java
@@ -14,7 +14,38 @@ public class Recursividad1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        System.out.println("Version 02: recursividades");
+
+        System.out.println("Factorial de 5: " + factorial(5));
+        System.out.println("Suma de 5: " + suma(5));
+        System.out.println("Fibonacci de 5: " + fibonacci(5));
     }
+
     
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+    
+    public static int suma(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return n + suma(n - 1);
+    }
+
+    
+    public static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1; 
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 }
